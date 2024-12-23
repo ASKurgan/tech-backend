97cc96677e745468837b655adebe21321808e6dd
kirill_sachkov <sachkovkrl@gmail.com>
2024-12-23 13:44:42 +0300

    some styling fixes

--- IssueService/src/Issues/SachkovTech.Issues.Domain/IssuesReviews/Entities/Comment.cs
+++ IssueService/src/Issues/SachkovTech.Issues.Domain/IssuesReviews/Entities/Comment.cs
@@ -7,15 +7,18 @@ namespace SachkovTech.Issues.Domain.IssuesReviews.Entities;
 
 public class Comment : Entity<CommentId>
 {
-    //Ef core
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-    private Comment(CommentId id) : base(id) {}
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    // Ef core
+    private Comment(CommentId id)
+        : base(id)
+    {
+    }
 
-    private Comment(CommentId id,
+    private Comment(
+        CommentId id,
         UserId userId,
         Message message,
-        DateTime createdAt) : base(id)
+        DateTime createdAt)
+        : base(id)
     {
         UserId = userId;
         Message = message;
@@ -23,13 +26,15 @@ public class Comment : Entity<CommentId>
     }
 
     public IssueReview? IssueReview { get; private set; }
-    public UserId UserId { get; private set; }
 
-    public Message Message { get; private set; }
+    public UserId UserId { get; private set; } = null!;
+
+    public Message Message { get; private set; } = null!;
 
     public DateTime CreatedAt { get; private set; }
 
-    public static Result<Comment, Error> Create(UserId userId,
+    public static Result<Comment, Error> Create(
+        UserId userId,
         Message message)
     {
         return Result.Success<Comment, Error>(new Comment(
