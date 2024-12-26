146511006e65feece43dcac9b898ce787befd44e
kirill_sachkov <sachkovkrl@gmail.com>
2024-12-26 20:43:54 +0300

    change docker files

--- AccountService/src/AccountService.Api/Program.cs
+++ AccountService/src/AccountService.Api/Program.cs
@@ -13,27 +13,6 @@ var app = builder.Build();
 
 await app.Configure();
 
-app.MapGet("/jwk", async () =>
-{
-    var rsaKey = RSA.Create();
-    rsaKey.ImportRSAPrivateKey(await File.ReadAllBytesAsync("etc/key"), out _);
-
-    var publicKey = RSA.Create();
-    publicKey.ImportRSAPublicKey(rsaKey.ExportRSAPublicKey(), out _);
-    var key = new RsaSecurityKey(publicKey);
-
-    return JsonWebKeyConverter.ConvertFromRSASecurityKey(key);
-});
-
-app.MapGet("/jwk-private", async () =>
-{
-    var rsaKey = RSA.Create();
-    rsaKey.ImportRSAPrivateKey(await File.ReadAllBytesAsync("etc/key"), out _);
-    var key = new RsaSecurityKey(rsaKey);
-
-    return JsonWebKeyConverter.ConvertFromRSASecurityKey(key);
-});
-
 app.Run();
 
 namespace ProjectTemplate
